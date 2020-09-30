f9f52d8348cfd039b26ae728b26ba9465b9fb6f8
Alfred Wang <alfredwang0707@gmail.com>
2020-09-30 21:53:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,6 @@ def using_delete(instructors,no_offense_steven)
   instructors.delete(steven)
 end
 
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots, deleted_robot)
   famous_robots.delete_at(2)
 end
