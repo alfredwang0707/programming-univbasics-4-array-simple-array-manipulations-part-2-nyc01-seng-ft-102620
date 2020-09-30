65c8435de072aaa3df73fcbab0df2542e9efa352
Alfred Wang <alfredwang0707@gmail.com>
2020-09-30 21:45:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,5 @@ my_favorite_things.concat(more_favs)
 def using_insert(list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4, another_language)
 end
+
+def using_uniq
